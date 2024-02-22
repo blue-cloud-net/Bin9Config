2ec7f75e4966e8fe1e9e250139abcb52470de73f
sky <bluecloudnet@outlook.com>
2024-02-22 01:25:09 +0000

    feat:添加数据库项目和UI项目

---
+++ src/Bind9Config.Manager/Bind9Config.Manager.EFCore/DbContext.cs
@@ -0,0 +1,6 @@
+﻿namespace Bind9Config.Manager.EFCore;
+
+public class Bind9ConfigDbContext : DbContext
+{
+    public DbSet<> MyProperty { get; set; }
+}
---
+++ src/Bind9Config.Manager/Bind9Config.Manager.UI/Components/Layout/NavMenu.razor.cs
@@ -0,0 +1,9 @@
+using Microsoft.AspNetCore.Components;
+
+
+namespace Bind9Config.Manager.UI.Components.Layout;
+
+public partial class NavMenu : ComponentBase
+{   
+     private List<MenuItem>? Menus { get; set; }
+}
